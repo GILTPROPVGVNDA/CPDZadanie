9aca3ab378213e73a6ee22d5acefcf7282785ac8
GILTPROPVGVNDA <115945890+GILTPROPVGVNDA@users.noreply.github.com>
2023-11-01 20:23:01 +0000

    Add files via upload

--- skrypt.sh
+++ skrypt.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#! /bin/bash
 
-sudo yum check-update -y
-sudo yum upgrade -y 
+sudo yum check-update
+sudo yum upgrade
 sudo systemctl daemon-reload
-sudo curl -sL "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -sl "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +755 /usr/local/bin/docker-compose
-docker-compose -v
