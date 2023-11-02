ee21aa9d8d331d0f20b8cfb98129b51e73a0afdb
GILTPROPVGVNDA <115945890+GILTPROPVGVNDA@users.noreply.github.com>
2023-11-02 22:59:33 +0000

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
 sudo yum check-update
 sudo yum upgrade
 sudo systemctl daemon-reload
-sudo curl -sl "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -sl "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +755 /usr/local/bin/docker-compose
